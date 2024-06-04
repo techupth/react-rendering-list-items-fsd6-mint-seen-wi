8d8910fc0a0f64f15b26d71821901f96b1bd6bb7
MINT <mint.2978@gmail.com>
2024-06-04 15:25:14 +0700

    feat: add movie list

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,37 @@
 import "./App.css";
+import movies from "./data/movies.jsx";
 
 function App() {
   return (
     <div className="App">
       <section className="movie-list-section">
-        {/* Render Movie Lists Here */}
+        <div className="app-movie">
+          <h1>Movie List Section</h1>
+          {movies.map((item, index) => (
+            <div key={index} className={`movie-list`}>
+              <div key={item.movies} className={`movie-block`}>
+                <div key={item.image} className={`movie-pic`}>
+                  <img src={item.image} />
+                </div>
+                <div key={`${item.movies}-name`} className={`movie-detail`}>
+                  <div>Title: {item.title} </div>
+                  <div>Year: {item.year}</div>
+                  <div>Runtime: {item.runtime}</div>
+                  <div className={`button-genre`}>
+                    Genres:{" "}
+                    {item.genres.map((genre, index) => (
+                      <button key={index} c>
+                        {genre}
+                      </button>
+                    ))}
+                  </div>
+                  <div>IMDB Rating: {item.imdbRating}</div>
+                  <div>IMDB Votes: {item.imdbVotes}</div>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
       </section>
     </div>
   );
