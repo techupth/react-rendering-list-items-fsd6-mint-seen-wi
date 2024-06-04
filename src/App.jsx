25c849d44e642cce15fd0662145027de0f8d2aa0
S33N <145544074+rizzboislw@users.noreply.github.com>
2024-06-04 14:52:08 +0700

    feat: created a list of movies with description

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
 import "./App.css";
+import movies from "./data/movies";
+
 
 function App() {
   return (
     <div className="App">
       <section className="movie-list-section">
-        {/* Render Movie Lists Here */}
+        { movies.map((item, index) => {
+        return ( 
+          <div key={index} className="movie-box">
+              <img className="movie-img" src={item.image}/>
+              <ul className="movie-data">
+              <li>Title: {item.title}</li>
+              <li>Year: {item.year}</li>
+              <li>Runtime: {item.runtime}</li>
+              <li>Genres:  
+                <span className="movie-genres">{item.genres[0]}</span>
+                <span className="movie-genres">{item.genres[1]}</span>
+                </li>
+              <li>IMDB Ratings: {item.imdbRating} </li>
+              <li>IMDB Votes: {item.imdbVotes}</li>
+              </ul>
+          </div>
+        )
+      }) }
       </section>
     </div>
   );
